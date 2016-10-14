4d4f38b2e2abbe85c9b0377dd6c185e1466024aa
Tim Maddux <timothymaddux@gmail.com>
2016-10-14 17:24:06 -0500

    Files added. Initial migration.

--- RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
+++ RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RepoQuiz.DAL;
 
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class StudentRepositoryTests
     {
+        //Set up Moq / mock context 
+        Mock<StudentContext> mock_context { get; set; }
+
+
+        [TestInitialize]
+        //mock_context = new Mock<StudentContext>();
     }
 }
---
+++ RepoQuiz/DAL/StudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RepoQuiz.Models;
+
+namespace RepoQuiz.DAL
+{
+    public class StudentRepo
+    {
+
+    }
+}
