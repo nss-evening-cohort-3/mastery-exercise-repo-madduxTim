ef37c3f00af3df5ce4a5861809b8af3485516f01
Tim Maddux <timothymaddux@gmail.com>
2016-10-16 09:31:42 -0500

    Seed method test. Need to refactor.

--- RepoQuiz/Migrations/Configuration.cs
+++ RepoQuiz/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace RepoQuiz.Migrations
 {
+    using DAL;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using RepoQuiz.DAL;
 
     internal sealed class Configuration : DbMigrationsConfiguration<RepoQuiz.DAL.StudentContext>
     {
@@ -14,6 +17,25 @@ namespace RepoQuiz.Migrations
 
         protected override void Seed(RepoQuiz.DAL.StudentContext context)
         {
+            NameGenerator name_generator = new NameGenerator();
+
+
+            context.Students.AddOrUpdate(
+                s => s.LastName, new Models.Student() { FirstName = "Test", LastName="TestLast", Major="Science" }
+
+                );
+            // HOW TO MAKE USE OF THIS WITH SEED METHOD... 
+            //List<string> student1 = name_generator.studentAssembly();
+            //List<string> student2 = name_generator.studentAssembly();
+            //List<string> student3 = name_generator.studentAssembly();
+            //List<string> student4 = name_generator.studentAssembly();
+            //List<string> student5 = name_generator.studentAssembly();
+            //List<string> student6 = name_generator.studentAssembly();
+            //List<string> student7 = name_generator.studentAssembly();
+            //List<string> student8 = name_generator.studentAssembly();
+            //List<string> student9 = name_generator.studentAssembly();
+            //List<string> student10 = name_generator.studentAssembly();
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
